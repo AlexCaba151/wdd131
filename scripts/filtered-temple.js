b98bf14ca50358ff508a2ac718c82f99ac75bfec
Yerlin A. Rodriguez <yerlin.alec21@gmail.com>
2025-02-04 14:21:13 -0400

    form using javascript

--- scripts/filtered-temple.js
+++ scripts/filtered-temple.js
@@ -129,7 +129,6 @@ function createTempleCard(filteredTemples) {
   });
 }
 
-
 createTempleCard(temples);
 
 
---
+++ scripts/form.js
@@ -0,0 +1,29 @@
+const products = [
+    { id: 1, name: "apple iphone 14" },
+    { id: 2, name: "samsung galaxy s23" },
+    { id: 3, name: "google pixel 7" },
+  ]
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    // Populate product select options
+    const productSelect = document.getElementById("product")
+  
+    products.forEach((product) => {
+      const option = document.createElement("option")
+      option.value = product.id
+      option.textContent = product.name
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+      productSelect.appendChild(option)
+    })
+  })
+  
+
+    // Copyright year
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Last modified date
+document.getElementById('lastModified').textContent = document.lastModified;
+
+  
